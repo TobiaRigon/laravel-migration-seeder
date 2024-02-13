2ec0e4c8305763e6278c4697d65a72bbc86e8094
TobiaRigon <rigontobia@gmail.com>
2024-02-13 16:34:02 +0100

    Feat: Faker + Seeader

--- database/factories/TrainFactory.php
+++ database/factories/TrainFactory.php
@@ -17,7 +17,15 @@ class TrainFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'company' => fake() ->company,
+            'departure_station' => fake() ->city,
+            'arrival_station' => fake() ->city,
+            'departure_time' => fake() ->time(),
+            'arrival_time' => fake() ->time(),
+            'train_code' => fake() ->unique()->randomNumber(5),
+            'carriage_count' => fake() ->numberBetween(5, 15),
+            'on_time' => fake() ->boolean(80), // 80% probabilità di essere in orario
+            'cancelled' => fake() ->boolean(5), // 5% probabilità di essere cancellato
         ];
     }
 }
--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -4,19 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
 
-class DatabaseSeeder extends Seeder
+class TrainTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-
-        $this -> call([
-            TrainTableSeeder :: class,
-        ]);
+        Train :: factory() -> count(30) -> create();
     }
 }
