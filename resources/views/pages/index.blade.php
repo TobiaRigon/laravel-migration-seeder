aa98bd448864679604787add2ed9cc1d329456c5
TobiaRigon <rigontobia@gmail.com>
2024-02-13 16:36:28 +0100

    Feat: Bootstrap cards in index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,9 +2,32 @@
 @section('head')
     <title>Home</title>
 @endsection
+
 @section('content')
     <h1>Trains: {{count($trains)}}</h1>
-    @foreach ($trains as $train)
-        <li> {{$train -> company}}</li>
-    @endforeach
+    <div class="row">
+        @foreach ($trains as $train)
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$train->company}}</h5>
+                        <p class="card-text">Departure: {{$train->departure_station}}</p>
+                        <p class="card-text">Arrival: {{$train->arrival_station}}</p>
+                        <p class="card-text">Departure Time: {{$train->departure_time}}</p>
+                        <p class="card-text">Arrival Time: {{$train->arrival_time}}</p>
+                        <p class="card-text">Train Code: {{$train->train_code}}</p>
+                        <p class="card-text">Carriage Count: {{$train->carriage_count}}</p>
+                        @if ($train->on_time)
+                            <p class="card-text">Status: On Time</p>
+                        @else
+                            <p class="card-text">Status: Delayed</p>
+                        @endif
+                        @if ($train->cancelled)
+                            <p class="card-text">Status: Cancelled</p>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
